0585acee27ae01d9655f30e016ad42538d25d945
mel <105978905+codeize1@users.noreply.github.com>
2024-05-02 21:21:32 +0530

    Update mains.js

--- mains.js
+++ mains.js
@@ -1,9 +1,9 @@
 // Get the current page URL
 const currentPage = window.location.pathname;
 const pageNames = {
-  '/': 'Home',
-  '/posts/post.html': 'Posts',
-  '/about/about.html': 'About',
+  '/': 'home',
+  '/posts/post.html': 'posts',
+  '/about/about.html': 'about',
   '/posts/blog1.html': 'welcome'
 };
 
@@ -13,14 +13,14 @@ function generateBreadcrumbs() {
   const pathSegments = currentPage.split('/').filter(
       segment => segment !== '');  // Split the URL and remove empty segments
 
-  let breadcrumbPath = '<a href="/">Home</a>';
+  let breadcrumbPath = '<a href="/">home</a>';
 
   for (let i = 1; i < pathSegments.length; i++) {
     const path = `/${pathSegments.slice(0, i + 1).join('/')}`;
     let pageName = pageNames[path] || pathSegments[i];
 
     if (path === '/posts/blog1.html') {
-      breadcrumbPath += ` / <a href="/posts/post.html">Posts</a> / ${pageName}`;
+      breadcrumbPath += ` / <a href="/posts/post.html">posts</a> / ${pageName}`;
     } else {
       breadcrumbPath += ` / <a href="${path}">${pageName}</a>`;
     }
